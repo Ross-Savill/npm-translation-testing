dfbbe4cfb0befaddc0e02501c5649e0e1b3acbb6
Ross Savill <rosssavill252@hotmail.com>
2019-01-11 17:08:17 +1100

    small change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const translate = require('translate');
 translate.engine = 'yandex';
 translate.key = process.env.DB_PASS;
 
-translate('I am sitting with Harrison in a room.', 'es').then(text => {
+translate('I am sitting with Harrison in a room.', 'mi').then(text => {
     console.log(text);
   });
 
